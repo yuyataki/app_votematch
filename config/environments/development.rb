7c678757796165c2147ad21761ed59402b79edeb
yuyataki <takiyour@gmail.com>
2017-01-21 17:40:56 +0900

    config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,8 +20,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :user_name => "takidragons@gmail.com",
-    :password => "sattsum20",
     :authentication => :plain,
     :enable_starttls_auto => true
   }
