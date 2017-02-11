c24b9f9ff0aaac3d509919d5ccfa153db1569fcb
yuyataki <takiyour@gmail.com>
2017-02-11 12:10:42 +0900

    add npm commands to ebextensions

--- frontend/config/webpack.production.js
+++ frontend/config/webpack.production.js
@@ -1,3 +1,6 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+
 module.exports = {
   entry: {
     application: './src/javascripts/application.js',
@@ -6,12 +9,24 @@ module.exports = {
     path: '../app/assets/javascripts',
     filename: '[name].js'
   },
+  plugins: [
+    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' })
+  ],
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel?presets[]=react,presets[]=es2015,presets[]=stage-2'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      {
+        test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
+        loader: 'url-loader?mimetype=application/font-woff'
       }
     ]
   }
